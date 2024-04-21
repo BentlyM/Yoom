5ba79c7db45e287c668a9a24eb86efa534b3f690
BentlyM <metayerbently@gmail.com>
2024-04-21 10:41:26 -0400

    "Add MeetingTypeList component to Home page; update tailwind config with new colors"

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import MeetingTypeList from '@/components/MeetingTypeList';
+
 
 const Home = () => {
   const now = new Date();
@@ -21,7 +22,7 @@ const Home = () => {
         </div>
       </div>
 
-
+      <MeetingTypeList />
     </section>
   )
 }
